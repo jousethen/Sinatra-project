ee45a208a7093371a98befa3833507701778e421
jousethen <jthen93@gmail.com>
2021-05-18 15:38:36 -0400

    Added Stylesheet and public folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < Sinatra::Base
 
   configure do 
     set :views, 'app/views'
+    set :public_folder, 'public'
     enable :sessions 
     set :sessions_secret, "password_security"
   end
