407e0849390a86e36ced7c35175acb768a3fac9e
jousethen <jthen93@gmail.com>
2021-05-18 09:24:15 -0400

    set up environment properly

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
     belongs_to :teacher
     has_many :student_courses
-    has_many :students through: :student_courses
+    has_many :students, through: :student_courses
 
     extend Slugfiable::ClassMethods
     include Slugfiable::InstanceMethods
