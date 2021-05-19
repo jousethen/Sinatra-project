4cf3fa70beee0c8e8ab446158b9e901c6c1c4910
jousethen <jthen93@gmail.com>
2021-05-19 12:17:34 -0400

    Completed Get /Login but Post Login is not working

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,4 +6,25 @@ class TeachersController < ApplicationController
       erb :"teachers/login"
     end
   end
+
+  get '/login' do
+    if session[:user_id]
+      erb :"teachers/index"
+    else
+      erb :"teachers/login"
+    end 
+    
+  end
+
+  post '/login' do
+    teacher = Teacher.find(params[:id])
+    binding.pry
+    if teacher && teacher.authenticate(params[:password])
+      session[:user_id] = teacher.id
+      redirect '/index'
+    else
+      redirect '/login'
+    end
+  end
+
 end
