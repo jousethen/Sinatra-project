3a013f986cea9834667943f9f60d85f78dc2c329
jousethen <jthen93@gmail.com>
2021-05-20 15:27:08 -0400

    Added Error Handling for Patch Courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,7 +17,7 @@ class CoursesController < ApplicationController
       @course = Course.find_by_slug(params[:slug])
       erb :"courses/show"
     else
-      erb :"teachers/login"
+      redirect "/"
     end
   end
 
@@ -34,7 +34,8 @@ class CoursesController < ApplicationController
   post '/courses/new' do 
     course = Course.find_by(name: params[:course][:name])
       if course 
-        redirect "/courses/#{course.slug}"
+        flash[:message] = "Course Name Already Taken"
+        redirect "/courses/new"
       else
         course = Course.create(name: params[:course][:name], teacher_id: session[:user_id])
 
@@ -47,13 +48,23 @@ class CoursesController < ApplicationController
   end
 
   patch '/courses/:slug' do
-    course = Course.find_by_slug(params[:slug])
+    # check if course name already exists
+    exist = Course.find_by(name: params[:course][:name])
+    if exist && exist.teacher_id != session[:user_id]
+      flash[:message] = "Course name already exists. Please choose a different name."
+      redirect "/courses/#{course.slug}/edit"
+    end
 
+    # find currently worked on course
+    course = Course.find_by_slug(params[:slug])
     student_courses = []
     
+    # get student_courses
     params[:course][:students].each do |student|
       student_courses  << StudentCourse.create(student_id: student, course_id: course.id)
     end
+    
+    # update course
     course.update(name: params[:course][:name], student_courses: student_courses)
     course.save
 
