f76e5f2043252afb352dd7f727ac7efcef7486b5
jousethen <jthen93@gmail.com>
2021-05-20 11:25:53 -0400

    Added seeds.rb for some students

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,11 @@
 class CoursesController < ApplicationController
+  get '/courses/new' do
+    if session[:user_id]
+      erb :"courses/new"
+    else
+      erb :"teachers/login"
+    end
+  end
+
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Student.create(name: "Tom Johnson", grade: 40)
+Student.create(name: "Abby Gray", grade: 60)
+Student.create(name: "Alexander the Good", grade: 81)
+Student.create(name: "Peter Parkour", grade: 91)
